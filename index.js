e0edc8cca51efa50b30d42c04e5fbd6ca1a65e6c
Aaron Smith <projects@aaronsmith.co>
2016-10-23 19:02:03 -0700

    argh

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 var bindings = require('bindings');
 
-// var pwm = 
-
-// var obj = new addon;
-// console.log( obj.setPWM(1.1, 1.1, 1.1, 1.1) );
-// console.log( obj.setPWM(1.2, 1.2, 1.2, 1.2) );
-// console.log( obj.setPWM(0.9, 0.9, 0.9, 0.9) );
-
 module.exports = {
   pwmController: function(){
     return new bindings('pwm.node').PWM();
