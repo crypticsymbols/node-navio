c0396cf951b3cb9592cabf1693d8b48985b14dab
Aaron Smith <projects@aaronsmith.co>
2016-10-27 17:28:46 -0700

    make runnable on OSX

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-var bindings = require('bindings');
+if (process.platform == 'linux' && process.env.NODE_ENV == 'production'){
+  // Is there a better way to target Raspberry Pi?
+  var bindings = require('bindings');
+}
 
 module.exports = {
+
   pwmController: function(){
-    return new bindings('pwm.node').PWM();
+    try {
+      return new bindings('pwm.node').PWM();
+    } catch(e){
+      return {
+        setPWM: function(){
+          console.log('not on OSX :)')
+        }
+      }
+    }
   }
 }
