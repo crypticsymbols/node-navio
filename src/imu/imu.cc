cbf2e96e1a194b0dd61ea22581ebbb1f4433d713
Aaron Smith <projects@aaronsmith.co>
2016-10-30 16:04:36 -0700

    fix IMU data return value keys

--- src/imu/imu.cc
+++ src/imu/imu.cc
@@ -63,15 +63,15 @@ void IMU::getIMU(const FunctionCallbackInfo<Value>& args) {
 
   obj->getCurrentData();
 
-  ret->Set(String::NewFromUtf8(isolate, 'ax'), Number::New(isolate, obj->ax));
-  ret->Set(String::NewFromUtf8(isolate, 'ay'), Number::New(isolate, obj->ay));
-  ret->Set(String::NewFromUtf8(isolate, 'az'), Number::New(isolate, obj->az));
-  ret->Set(String::NewFromUtf8(isolate, 'gx'), Number::New(isolate, obj->gx));
-  ret->Set(String::NewFromUtf8(isolate, 'gy'), Number::New(isolate, obj->gy));
-  ret->Set(String::NewFromUtf8(isolate, 'gz'), Number::New(isolate, obj->gz));
-  ret->Set(String::NewFromUtf8(isolate, 'mx'), Number::New(isolate, obj->mx));
-  ret->Set(String::NewFromUtf8(isolate, 'my'), Number::New(isolate, obj->my));
-  ret->Set(String::NewFromUtf8(isolate, 'mz'), Number::New(isolate, obj->mz));
+  ret->Set(String::NewFromUtf8(isolate, "ax"), Number::New(isolate, obj->ax));
+  ret->Set(String::NewFromUtf8(isolate, "ay"), Number::New(isolate, obj->ay));
+  ret->Set(String::NewFromUtf8(isolate, "az"), Number::New(isolate, obj->az));
+  ret->Set(String::NewFromUtf8(isolate, "gx"), Number::New(isolate, obj->gx));
+  ret->Set(String::NewFromUtf8(isolate, "gy"), Number::New(isolate, obj->gy));
+  ret->Set(String::NewFromUtf8(isolate, "gz"), Number::New(isolate, obj->gz));
+  ret->Set(String::NewFromUtf8(isolate, "mx"), Number::New(isolate, obj->mx));
+  ret->Set(String::NewFromUtf8(isolate, "my"), Number::New(isolate, obj->my));
+  ret->Set(String::NewFromUtf8(isolate, "mz"), Number::New(isolate, obj->mz));
 
   args.GetReturnValue().Set(ret);
 }
