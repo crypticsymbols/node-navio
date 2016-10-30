a018cd9eab4fcc5dcdadc3a842410361b43826e2
Aaron Smith <projects@aaronsmith.co>
2016-10-30 14:58:57 -0700

    ugh

--- src/imu/imu.cc
+++ src/imu/imu.cc
@@ -80,7 +80,7 @@ void IMU::getIMU(const FunctionCallbackInfo<Value>& args) {
   // float ms_3 = args[2]->IsUndefined() ? 0 : args[2]->NumberValue();
   // float ms_4 = args[3]->IsUndefined() ? 0 : args[3]->NumberValue();
 
-  Local<Array> ret = Array::New(); 
+  Local<Array> ret = Array::New(isolate, 1); 
 ret.Set(0, String::New("őúűáéöüó")); 
 
 return scope.Close(ret); 
