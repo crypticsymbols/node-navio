33ee6f9393371eceeb2389cc33939ef7f4dc99c1
Aaron Smith <projects@aaronsmith.co>
2016-10-30 15:56:00 -0700

    return IMU data to node addon

--- src/imu/imu.cc
+++ src/imu/imu.cc
@@ -53,30 +53,25 @@ void IMU::New(const FunctionCallbackInfo<Value>& args) {
 
 void IMU::getCurrentData() {
   this->imu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
-  printf("Acc: %+7.3f %+7.3f %+7.3f  ", ax, ay, az);
-  printf("Gyr: %+8.3f %+8.3f %+8.3f  ", gx, gy, gz);
-  printf("Mag: %+7.3f %+7.3f %+7.3f\n", mx, my, mz);
 }
 
 void IMU::getIMU(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = Isolate::GetCurrent();
   HandleScope scope(isolate);
   IMU* obj = ObjectWrap::Unwrap<IMU>(args.Holder());
+  Local<Object> ret = Object::New(isolate); 
 
-  // float channel = args[0]->IsUndefined() ? 0 : args[0]->NumberValue();
-  // float ms = args[1]->IsUndefined() ? 0 : args[1]->NumberValue();
-  // float ms_3 = args[2]->IsUndefined() ? 0 : args[2]->NumberValue();
-  // float ms_4 = args[3]->IsUndefined() ? 0 : args[3]->NumberValue();
   obj->getCurrentData();
 
-  Local<Object> ret = Object::New(isolate); 
-  ret->Set('ax', Number::New(isolate, obj->ax)); 
-  ret->Set('ay', obj->ay); 
-  ret->Set('az', 0); 
-
-// return scope.Close(ret); 
-
-  // float derp;
+  ret->Set('ax', Number::New(isolate, obj->ax));
+  ret->Set('ay', Number::New(isolate, obj->ay));
+  ret->Set('az', Number::New(isolate, obj->az));
+  ret->Set('gx', Number::New(isolate, obj->gx));
+  ret->Set('gy', Number::New(isolate, obj->gy));
+  ret->Set('gz', Number::New(isolate, obj->gz));
+  ret->Set('mx', Number::New(isolate, obj->mx));
+  ret->Set('my', Number::New(isolate, obj->my));
+  ret->Set('mz', Number::New(isolate, obj->mz));
 
   args.GetReturnValue().Set(ret);
 }
