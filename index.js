68cb7d12af75bbf1285f424357e967c78c3d7e70
Aaron Smith <projects@aaronsmith.co>
2016-11-13 18:12:14 -0800

    cleanup lint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-if (process.platform == 'linux'){
+if (process.platform === 'linux') {
   // Is there a better way to target Raspberry Pi?
-  var bindings = require('bindings');
+  var bindings = require('bindings')
   var p = bindings('pwm.node')
   var i = bindings('imu.node')
   var a = bindings('ahrs.node')
@@ -11,37 +11,35 @@ module.exports = {
   pwmController: function() {
     try {
       return new p.PWM();
-    } catch(e){
+    } catch (e) {
       return {
-        setPWM: function(channel, uS){
+        setPWM: function(channel, uS) {
           // console.log('PWM Command: Channel '+channel+' set to '+uS+'uS')
         }
       }
     }
   },
-  
-  ahrsData: function(){
+  ahrsData: function() {
     try {
       return new a.AHRS();
-    } catch(e){
+    } catch (e) {
       return {
-        getData: function(){
+        getData: function() {
           return {
-            ax: 0, ay: 0, az: 0, gx: 0, gy: 0, gz: 0, mx: 0, my: 0, mz: 0,
+            ax: 0, ay: 0, az: 0, gx: 0, gy: 0, gz: 0, mx: 0, my: 0, mz: 0
           }
         }
       }
     }
   },
-
-  imuReader: function(){
+  imuReader: function() {
     try {
       return new i.IMU();
-    } catch(e){
+    } catch (e) {
       return {
-        getIMU: function(){
+        getIMU: function() {
           return {
-            ax: 0, ay: 0, az: 0, gx: 0, gy: 0, gz: 0, mx: 0, my: 0, mz: 0,
+            ax: 0, ay: 0, az: 0, gx: 0, gy: 0, gz: 0, mx: 0, my: 0, mz: 0
           }
         }
       }
