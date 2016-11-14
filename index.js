e482ebadd80726f0c83dad084bd9ae390c164386
Aaron Smith <projects@aaronsmith.co>
2016-11-13 18:05:28 -0800

    accessor function added to AHRS

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 if (process.platform == 'linux'){
   // Is there a better way to target Raspberry Pi?
   var bindings = require('bindings');
-  var p = require('bindings')('pwm.node')
-  var i = require('bindings')('imu.node')
-  var a = require('bindings')('ahrs.node')
+  var p = bindings('pwm.node')
+  var i = bindings('imu.node')
+  var a = bindings('ahrs.node')
 }
 
 module.exports = {
 
-  pwmController: function(){
+  pwmController: function() {
     try {
       return new p.PWM();
     } catch(e){
@@ -21,17 +21,17 @@ module.exports = {
   },
   
   ahrsData: function(){
-    // try {
-      // return new i.IMU();
-    // } catch(e){
-      // return {
-        // getIMU: function(){
-          // return {
-            // ax: 0, ay: 0, az: 0, gx: 0, gy: 0, gz: 0, mx: 0, my: 0, mz: 0,
-          // }
-        // }
-      // }
-    // }
+    try {
+      return new a.AHRS();
+    } catch(e){
+      return {
+        getData: function(){
+          return {
+            ax: 0, ay: 0, az: 0, gx: 0, gy: 0, gz: 0, mx: 0, my: 0, mz: 0,
+          }
+        }
+      }
+    }
   },
 
   imuReader: function(){
