3cc2c5e6e2daa8620c77de6ab111186d614a3176
Aaron Smith <projects@aaronsmith.co>
2016-12-19 16:14:51 -0800

    gyp mpu9250 by self

--- src/mpu9250/addon.cc
+++ src/mpu9250/addon.cc
@@ -26,6 +26,10 @@ class MPU9250_Interface : public StreamingWorker {
 
     void Execute (const AsyncProgressWorker::ExecutionProgress& progress) {
 
+      float ax, ay, az;
+    float gx, gy, gz;
+    float mx, my, mz;
+    
       this->imu.initialize();
 
       int sendInterval = 10; // Interval results are sent in ms
@@ -50,9 +54,7 @@ class MPU9250_Interface : public StreamingWorker {
     }
   private:
     string name;
-    float ax, ay, az;
-    float gx, gy, gz;
-    float mx, my, mz;
+    
     MPU9250 imu;
 };
 
